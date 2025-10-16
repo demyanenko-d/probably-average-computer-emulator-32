e214c48235228c42cba39e3754264f84f3d96653
Charlie Birks <charlie@daft.games>
2025-10-16 13:45:32 +0100

    SLDT

--- core/CPU.cpp
+++ core/CPU.cpp
@@ -861,6 +861,24 @@ void RAM_FUNC(CPU::executeInstruction)()
 
                     switch(exOp)
                     {
+                        case 0x0: // SLDT
+                        {
+                            // not recognised in real/virtual-8086 mode
+                            if(!isProtectedMode() || (flags & Flag_VM))
+                            {
+                                fault(Fault::UD);
+                                break;
+                            }
+
+                            int cycles;
+
+                            // with 32bit operand size writing to mem only writes 16 bits
+                            // writing to reg leaves high 16 bits undefined
+                            writeRM16(modRM, ldtSelector, cycles, addr);
+
+                            reg(Reg32::EIP) += 2;
+                            break;
+                        }
                         case 0x1: // STR
                         {
                             // not recognised in real/virtual-8086 mode
